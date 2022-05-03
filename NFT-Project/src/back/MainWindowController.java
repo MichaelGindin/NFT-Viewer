eb5ae729cb7e8c977f6718ed6a27978b4f34de4b
Yaniv Sokolov <yansokolov96@gmail.com>
2022-05-03 10:53:16 +0300

    initialize table and combobox

--- NFT-Project/src/back/MainWindowController.java
+++ NFT-Project/src/back/MainWindowController.java
@@ -1,5 +1,9 @@
 package back;
 
+import java.time.LocalDate;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -8,6 +12,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 
 public class MainWindowController {
@@ -63,10 +68,35 @@ public class MainWindowController {
 	@FXML
 	private Button btnAddCollection;
 
+	ObservableList<NFTCollectionView> data = FXCollections.observableArrayList();
+
 	@FXML
 	void OnRefreshBtnTimerClick(ActionEvent event) {
 		int refreshTime = Integer.parseInt(txtRefreshTimer.getText());
 		System.out.println(refreshTime);
+
+		data.clear();
+		for (int i = 0; i < 150; i++) {
+
+			data.add(new NFTCollectionView("temp", new Float(5.5), new Float(5.5), new Float(i)));
+			data.add(new NFTCollectionView("temp1", new Float(5.5), new Float(5.5), new Float(i + 1)));
+			data.add(new NFTCollectionView("temp2", new Float(5.5), new Float(5.5), new Float(i + 1)));
+			data.add(new NFTCollectionView("temp3", new Float(5.5), new Float(5.5), new Float(i + 1)));
+		}
+		initializeTable();
+	}
+
+	private void initializeTable() {
+		collectionNameCol.setCellValueFactory(new PropertyValueFactory<NFTCollectionView, String>("collection_name"));
+		openseaCol.setCellValueFactory(new PropertyValueFactory<NFTCollectionView, Float>("opensea_price"));
+		magicEdenCol.setCellValueFactory(new PropertyValueFactory<NFTCollectionView, Float>("magic_eden_price"));
+		diffCol.setCellValueFactory(new PropertyValueFactory<NFTCollectionView, Float>("diff"));
+		collectionTableView.setItems(data);
+	}
+
+	public void setComboBox() {
+		cmboxEntries.getItems().addAll(10, 50, 100);
+		cmboxEntries.setValue(10);
 	}
 
 }
